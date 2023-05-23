806fe5a44544d09458a188fa73aa0cc357ac256c
shyamshyam018 <131571361+shyamshyam018@users.noreply.github.com>
2023-05-23 22:31:44 +0530

    Update stt.py

--- stt.py
+++ stt.py
@@ -1,15 +1,20 @@
 import streamlit as st
 import pyshorteners as pyst
 import pyperclip
-shortener=pyst.shortener()
+
+shortener = pyst.Shortener()
+
 def copying():
-    pyperclip.copy(shorted_url)
-st.markdown("<h1 style='text-align: center;'URL SHORTENER</h1>",unsafe_allow_html=True)
-form=st.form("name")
-url=form.text_input("URL HERE")
-s_btn=form.form_submit_button("SHORT")
+    pyperclip.copy(shortened_url)
+
+st.markdown("<h1 style='text-align: center;'>URL SHORTENER</h1>", unsafe_allow_html=True)
+
+form = st.form("name")
+url = form.text_input("URL HERE")
+s_btn = form.form_submit_button("SHORT")
+
 if s_btn:
-    shortened_url=shortener.tinyurl.short(url)
-    st.makrdown("<h3>Shorted Url</h3>" , unsafe_allow_html=True)
-    st.markdown(f"<h6 style='text-align;center;'>{shorted_url}</h6>",unsafe_allow_html=True)
-    st.button("Copy",on_click=copying)
+    shortened_url = shortener.tinyurl.short(url)
+    st.markdown("<h3>Shortened Url</h3>", unsafe_allow_html=True)
+    st.markdown(f"<h6 style='text-align: center;'>{shortened_url}</h6>", unsafe_allow_html=True)
+    st.button("Copy", on_click=copying)
