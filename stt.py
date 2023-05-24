142484595f84de8e7f066f85849d74b9b73d0b4c
shyamshyam018 <131571361+shyamshyam018@users.noreply.github.com>
2023-05-24 10:38:33 +0530

    Update stt.py

--- stt.py
+++ stt.py
@@ -1,7 +1,9 @@
 import streamlit as st
 from pydub import AudioSegment, silence
 import speech_recognition as sr
-recog=sr.Recognizer()
+
+recog = sr.Recognizer()
+final_result = ""
 
 st.markdown("<h1 style='text-align:center;'>AUDIO TO TEXT</h1>", unsafe_allow_html=True)
 st.markdown("---", unsafe_allow_html=True)
@@ -12,12 +14,15 @@ if audio:
     audio_segment = AudioSegment.from_file(audio)
     chunks = silence.split_on_silence(audio_segment, min_silence_len=500, silence_thresh=audio_segment.dBFS - 20,
                                       keep_silence=100)
-    for index,chunk in enumerate(chunks):
-        chunk.export(str(index)+".wav",format="wav")
-        with sr.AudioFile(str(index)+".wav") as source:
-            recorded=recog.record(source)
+    for index, chunk in enumerate(chunks):
+        chunk.export(str(index) + ".wav", format="wav")
+        with sr.AudioFile(str(index) + ".wav") as source:
+            recorded = recog.record(source)
             try:
-                text=recog.recognize_google(recorded)
-                print(text)
+                text = recog.recognize_google(recorded)
+                final_result += text
+                st.write(text)
             except:
-                print("None")
+                st.write("None")
+                final_result += "Unaudible"
+    st.text_area("", value=final_result)
