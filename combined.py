605735e4750d1d3abe0b75ebd651fa4aa3aa43df
shyamshyam018 <131571361+shyamshyam018@users.noreply.github.com>
2023-05-25 11:11:26 +0530

    Update combined.py

--- combined.py
+++ combined.py
@@ -84,4 +84,49 @@ def generate_html_download_link(fig):
     towrite = StringIO()
     fig.write_html(towrite, include_plotlyjs="cdn")
     towrite = BytesIO(towrite.getvalue().encode())
-    b64 = base64.b64
+    b64 = base64.b64encode(towrite.read()).decode()
+    href = f'<a href="data:text/html;charset=utf-8;base64, {b64}" download="plot.html">Download Plot</a>'
+    return st.markdown(href, unsafe_allow_html=True)
+
+st.sidebar.title('Navigation')
+selection = st.sidebar.radio("Go to", ('Description', 'Speech to Text', 'Excel Plotter'))
+
+if selection == 'Description':
+    st.header('Description')
+    st.write('This is a web app that allows you to convert speech to text and visualize data from an Excel file.')
+
+elif selection == 'Speech to Text':
+    st.header('Speech to Text')
+    st.write('Upload a WAV file containing speech and transcribe it.')
+    # Rest of the code for Speech to Text section
+
+elif selection == 'Excel Plotter':
+    st.header('Excel Plotter')
+    st.write('Feed the app with an Excel file and visualize the data.')
+    uploaded_file = st.file_uploader('Choose a XLSX file', type='xlsx')
+    if uploaded_file:
+        st.markdown('---')
+        df = pd.read_excel(uploaded_file, engine='openpyxl')
+        st.dataframe(df)
+        groupby_column = st.selectbox(
+            'What would you like to analyse?',
+            ('Ship Mode', 'Segment', 'Category', 'Sub-Category'),
+        )
+
+        output_columns = ['Sales', 'Profit']
+        df_grouped = df.groupby(by=[groupby_column], as_index=False)[output_columns].sum()
+
+        fig = px.bar(
+            df_grouped,
+            x=groupby_column,
+            y='Sales',
+            color='Profit',
+            color_continuous_scale=['red', 'yellow', 'green'],
+            template='plotly_white',
+            title=f'<b>Sales & Profit by {groupby_column}</b>'
+        )
+        st.plotly_chart(fig)
+
+        st.subheader('Downloads:')
+        generate_excel_download_link(df_grouped)
+        generate_html_download_link(fig)
